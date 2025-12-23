032376a930b059c0d6a0fefe28789b4ddeab1bc1
Takuya Akashi <tak.akashi@gmail.com>
2025-12-23 13:54:53 +0900

    deleted unnecessary files

--- run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /Users/tak/Business/Upstage/test-ocr-models
-uv run python src/utils/image_deskew.py --single datasets/appen_test_200/ja_pii_handwriting_0038.jpg --output datasets/test_corrected
